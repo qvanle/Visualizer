93a4b645355a7f485d035710b652d53878dddd9b
qvanle <vanle199tk@gmail.com>
2023-07-17 18:42:58 +0700

    add shape

--- include/GLOBAL.hpp
+++ include/GLOBAL.hpp
@@ -1,10 +1,20 @@
 #ifndef GLOBAL 
 #define GLOBAL 
 
+#include <random>
 enum class WINDOW_STATUS 
 {
     IS_OPEN,
     IS_CLOSED
+};
+enum class SHAPE 
+{
+    NONE,
+    RECTANGLE,
+    CIRCLE,
+    TRIANGLE,
+    LINE, 
+    POLYGON
 };
 
 #endif 
