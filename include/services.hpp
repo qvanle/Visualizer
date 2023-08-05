2ee18c18f850a430afa87e135537be8acb4eb5cc
qvanle <vanle199tk@gmail.com>
2023-08-05 18:52:53 +0700

    add random services

--- include/services.hpp
+++ include/services.hpp
@@ -4,8 +4,11 @@
 #include <string>
 #include <math.h>
 #include <climits>
+#include <random>
+#include <chrono>
 
 #include <nlohmann/json.hpp>
+
 using json = nlohmann::json;
 
 namespace JSON 
@@ -24,6 +27,24 @@ namespace NUMBER
     std::string removeLeadingZero(std::string s);
     bool isNumber(std::string s);
     bool isInInterval(std::string s, int64_t a, int64_t b);
+}
+
+namespace RANDOM 
+{
+    const std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+    int getInt(int a, int b);
+    long long getLongLong(long long a, long long b);
+    
+    float getFloat(float a, float b);
+    double getDouble(double a, double b);
+    
+    char getChar(char a, char b);
+    char getChar();
+    
+    std::string getString(int length);
+    std::string getString(int length, char a, char b);
+
+    bool flipCoin();
 }
 
 #endif 
---
+++ src/services/random.cpp
@@ -0,0 +1,52 @@
+#include <services.hpp>
+
+int RANDOM::getInt(int a, int b)
+{
+    return std::uniform_int_distribution<int> (a, b)(rng);
+}
+
+long long RANDOM::getLongLong(long long a, long long b)
+{
+    return std::uniform_int_distribution<long long> (a, b)(rng);
+}
+
+float RANDOM::getFloat(float a, float b)
+{
+    return std::uniform_real_distribution<float> (a, b)(rng);
+}
+
+double RANDOM::getDouble(double a, double b)
+{
+    return std::uniform_real_distribution<double> (a, b)(rng);
+}
+
+char RANDOM::getChar(char a, char b)
+{
+    return std::uniform_int_distribution<char> (a, b)(rng);
+}
+
+char RANDOM::getChar()
+{
+    return getChar('a', 'z');
+}
+
+std::string RANDOM::getString(int length)
+{
+    std::string s = "";
+    for (int i = 0; i < length; i++)
+        s += getChar();
+    return s;
+}
+
+std::string RANDOM::getString(int length, char a, char b)
+{
+    std::string s = "";
+    for (int i = 0; i < length; i++)
+        s += getChar(a, b);
+    return s;
+}
+
+bool RANDOM::flipCoin()
+{
+    return getInt(1, 2) - 1;
+}
